885c6b3095efdf731539c504ba23854eebf8427d
Maxim Trogaev <moriarty18.02@gmail.com>
2022-12-20 11:18:17 +0000

    EPD-12741 test commit

--- vars/getIssueIdFromCommitMessage.groovy
+++ vars/getIssueIdFromCommitMessage.groovy
@@ -1,6 +1,6 @@
 def call(Map config=[:]) {
   def rawIssueId = sh(returnStdout: true,script: "git log --oneline --format=%B -n 1 $GIT_COMMIT | head -n 1 | cut -d' ' -f1")
-  //echo "rawIssueId = ${rawIssueId}"
+  echo "rawIssueId = ${rawIssueId}"
   def issueId = rawIssueId.replaceAll("[^a-zA-Z0-9-]+","")
   if( issueId ==~ /((?<!([A-Za-z]{1,10})-?)[A-Z]+-\d+)/ ) {
     sh 'echo ${issueId}'
