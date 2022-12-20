c924ebbcd83968d79ce3faf7b816e62cc15f404a
Maxim Trogaev <moriarty18.02@gmail.com>
2022-12-20 11:29:23 +0000

    test commit

--- vars/addJiraLabel.groovy
+++ vars/addJiraLabel.groovy
@@ -1,9 +1,4 @@
 def call(Map config=[:]) {
-    // try {
-
-    // } catch() {
-
-    // }
     def testIssue = [update: [labels: [[add:"${config.label}"]]]]  
     response = jiraEditIssue idOrKey: "${config.issue}", issue: testIssue, site: "${config.site}"
 }
